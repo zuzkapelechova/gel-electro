a0a46b47439d229f50e95aa1d34448c5f77e2929
Nikola Svobodnik <n_svobodnik@lad.ceitec.muni.cz>
2025-11-21 16:24:50 +0100

    unfinished

---
+++ src/gelelectro/analysis/concentration/get_conc.py
@@ -0,0 +1,69 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from scipy import signal as sig
+from utils import *
+
+image = sys.argv[1] # preprocessed image
+ladder2 = sys.argv[2] # ladder on each side - True/False
+
+# read the preprocessed image
+png_image = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+cv2.imwrite('image.jpg', png_image) # to do: dlt this line, move to img preprocessing
+image = 'image.jpg'
+grayscale_img = cv2.imread(image, cv2.IMREAD_GRAYSCALE) # to fix: move to img preprocessing
+img_height, img_width = grayscale_img.shape
+
+# read the img into signals dictionary
+signals = get_signals_list(grayscale_img, img_width)
+
+# get positions of signal peaks per pixel line
+all_peak_centers = {}
+for index, signal in enumerate(signals):
+    all_peak_centers[index] = get_peak_centers(signals, index, 5, 20)
+
+# find ladder and lanes and save the specifications (start, center, end of each lane)
+all_lane_specifs, ladder_specifs = get_lane_and_ladder_specifs(width_threshold = 25, min_ladder_bands = 3, all_peak_centers = all_peak_centers)
+
+
+'''
+# get variables of the distance->size function
+slope, intercept, main_ladder_band_dist = get_sample_size_function_variables(signals, ladder_specifs, [1517, 1000, 517])
+
+
+if ladder2 == "True":
+    all_lane_specifs.popitem()  # rm the 2nd ladder from lanes dictionary
+
+# variable for cv2.putText()
+text_size = cv2.getTextSize("-----", cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)[0]
+
+# estimate the size of each sample
+with open("output.txt", "w") as output:
+    for lane in all_lane_specifs:
+        sample_distances = all_peak_centers[int(all_lane_specifs[lane]["center"])]
+        sample_sizes = []
+
+        output.write(f"lane {lane}: \n")
+        for i, sample_distance in enumerate(sample_distances):
+            sample_size = estimate_sample_size(slope, intercept, sample_distance)
+            sample_sizes.append(sample_size)
+
+            #write sizes into output file
+            output.write(f"{str(sample_size)} bp\n")
+
+            # add image annotations
+            centered_x_position = int(all_lane_specifs[lane]["center"]) - text_size[0] // 2
+            y_position = int(sample_distance)
+            cv2.putText(grayscale_img, f"{int(sample_size)} bp", (centered_x_position, 15 + 15*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(grayscale_img, "-----", (centered_x_position, sample_distance), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+
+        output.write("--------------\n")
+
+save_image(grayscale_img, "output.png")
+save_sample_size_function_plot(slope, intercept, main_ladder_band_dist, [1517, 1000, 517], "sample_size_function.png")
+'''
+get_peak_edges(all_lane_specifs, signals, all_peak_centers, 1713, intens_threshold=5)
+#get_intensities(all_lane_specifs, all_peak_centers, signals, 1713)
+mk_weight_fnc(ladder_specifs, all_peak_centers, signals, [45, 95, 97])
+plot_signal_in_pixel_line(signals, 72)
---
+++ src/gelelectro/analysis/concentration/utils.py
@@ -0,0 +1,198 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from scipy import signal as sig
+
+
+def save_image(img, output):
+    plt.figure()
+    plt.imshow(img, cmap = 'gray')
+    plt.axis('off')
+    #plt.show()
+    plt.savefig(output, dpi=300, bbox_inches='tight')
+
+def get_signals_list(img, img_width):
+    # returns list of signals (per pixel line)
+    signals = []
+
+    for pixel_line in range(img_width):
+        signal_line = img[:,pixel_line]
+        signals.append(signal_line)
+        
+    return signals
+
+def plot_signal_in_pixel_line(signals, pixel_line):
+    plt.figure()
+    plt.plot(signals[pixel_line])
+    plt.title(pixel_line)
+    plt.ylim(top=255)
+    plt.show()
+
+def get_peak_centers(signals, pixel_line, width_threshold, intensity_threshold):
+    signal = signals[pixel_line]
+    peak_centers = []
+    peaks, properties = sig.find_peaks(signal, height=intensity_threshold, width=width_threshold)
+
+    for peak in peaks:
+        peak_centers.append(peak)
+    
+    return peak_centers
+
+
+def get_lane_and_ladder_specifs(width_threshold, min_ladder_bands, all_peak_centers):
+    all_lane_specifs = {}
+    ladder_specifs = []
+    start = False
+    width = 0
+
+    for pixel_line in all_peak_centers:
+        num_of_peaks = len(all_peak_centers[pixel_line])
+
+        if num_of_peaks > 0 and not start:
+            lane_specifs = {}
+            start = pixel_line
+            width = 1
+                  
+        elif num_of_peaks > 0 and start:
+            width += 1 
+
+        elif num_of_peaks == 0 and (start or pixel_line == len(all_lane_specifs)-1):
+            if width > width_threshold:
+                lane_specifs["start"] = start
+                lane_specifs["center"] = (start + width/2)
+                lane_specifs["end"] = pixel_line
+
+                if len(ladder_specifs) == 0:
+                    ladder_specifs = lane_specifs
+                else:
+                    all_lane_specifs[len(all_lane_specifs)] = lane_specifs
+
+            width = 0
+            start = False
+
+    return all_lane_specifs, ladder_specifs
+    
+def get_peak_edges(all_lane_specifs, signals, all_peak_centers, pixel_line, intens_threshold, ladder_peak_centers = None):
+    signal = signals[pixel_line]
+
+    if ladder_peak_centers is not None:
+        peak_centers = ladder_peak_centers
+    else:
+        peak_centers = all_peak_centers[pixel_line]
+
+    starts = {}
+    ends = {}
+    
+    for peak_center in peak_centers:
+        top_intens = signal[peak_center]
+        start_pixel = end_pixel = peak_center
+        end_found = False
+        start_found = False
+        while not start_found or not end_found:
+            end_pixel += 1
+            start_pixel -= 1
+            if signal[end_pixel] <= intens_threshold and not end_found:
+                ends[peak_center] = end_pixel
+                end_found = True
+            if signal[start_pixel] <= intens_threshold and not start_found:
+                starts[peak_center] = start_pixel
+                start_found = True
+    return starts, ends
+
+
+'''
+def get_avg_intens(all_lane_specifs, lane, signals, all_peak_centers):
+    for pixel_line in range(all_lane_specifs[lane]["start"], all_lane_specifs[lane]["end"]):
+        
+        starts, ends = get_peak_edges(all_lane_specifs, signals, all_peak_centers, pixel_line, 5)
+        sums = {}
+
+        for peak_center in starts.keys():
+            sum[peak_center]
+'''
+def get_intensities(lanes_or_ladder_specifs, all_peak_centers, signals, pixel_line, ladder_peak_centers = None):
+    if ladder_peak_centers is not None:
+        starts, ends = get_peak_edges(lanes_or_ladder_specifs, signals, all_peak_centers, pixel_line, 5, ladder_peak_centers)
+    else:
+        starts, ends = get_peak_edges(lanes_or_ladder_specifs, signals, all_peak_centers, pixel_line, 5)
+    intensities = {}
+    for peak_center in starts.keys():
+        signal = signals[pixel_line]
+        intensity = sum(int(signal[pixel]) for pixel in range(starts[peak_center], ends[peak_center]))
+        intensities[peak_center] = intensity
+    return intensities
+
+def mk_weight_fnc(ladder_specifs, all_peak_centers, signals, weights):
+
+    ladder_center_signal = signals[int(ladder_specifs["center"])]
+    distances, properties = sig.find_peaks(ladder_center_signal, height=5, distance = 5, prominence=5)
+        
+    if len(distances) > 3:  # doubled - mk fnc()
+    # Get heights of detected peaks # doubled - mk fnc()
+        peak_heights = properties['peak_heights']   # doubled - mk fnc()
+        top_indices = np.argsort(peak_heights)[-3:] # doubled - mk fnc()
+    # doubled - mk fnc()
+        # Select the highest 3 peaks    # doubled - mk fnc()
+        top_peak_distances = sorted(distances[top_indices]) # doubled - mk fnc()
+    elif len(distances) == 3:   # doubled - mk fnc()
+        top_peak_distances = sorted(distances)  # If less than 3 peaks, take whatever is found  # doubled - mk fnc()
+    else:   # doubled - mk fnc()
+        raise ValueError("Couldn't find 3 main ladder bands")   # doubled - mk fnc()
+
+    intensities = get_intensities(ladder_specifs, all_peak_centers, signals, int(ladder_specifs["center"]), top_peak_distances)
+    print(intensities)
+    
+    values = [intensities[124], intensities[152], intensities[188]]
+
+    plt.scatter(values, weights)
+    plt.show()
+
+def get_band_distances_of_lane(signals, all_lane_specifs, lane):
+    center_pixel_line = int(all_lane_specifs[lane]["center"])
+    center_band_distances = np.array(signals[center_pixel_line])
+    return center_band_distances
+
+def get_sample_size_function_variables(signals, ladder_specifs, sizes):
+    ladder_center_signal = signals[int(ladder_specifs["center"])]
+    distances, properties = sig.find_peaks(ladder_center_signal, height=5, distance = 5, prominence=5)
+
+    if len(distances) > 3:
+        # Get heights of detected peaks
+        peak_heights = properties['peak_heights']
+        top_indices = np.argsort(peak_heights)[-3:]
+
+        # Select the highest 3 peaks
+        top_peak_distances = sorted(distances[top_indices])
+    elif len(distances) == 3:
+        top_peak_distances = sorted(distances)  # If less than 3 peaks, take whatever is found
+    else:
+        raise ValueError("Couldn't find 3 main ladder bands")
+
+
+    log_sizes = np.log10(sizes)
+
+    slope, intercept = np.polyfit(top_peak_distances, log_sizes, 1)
+
+    return slope, intercept, top_peak_distances
+
+def save_sample_size_function_plot(slope, intercept, top_peak_distances, sizes, output):
+    
+    x = np.linspace(min(top_peak_distances), max(top_peak_distances), 100)
+
+    # Compute corresponding y-values using the linear function
+    y = slope * x + intercept
+
+    # Plot the data points and the fitted line
+    plt.figure()
+    plt.scatter(top_peak_distances, np.log10(sizes))
+    plt.plot(x, y, color='red')
+    plt.xlabel('Distance [px]')
+    plt.ylabel('log10(size)')
+    plt.title('SIZE - DISTANCE')
+    #plt.show()
+    plt.savefig(output)
+
+def estimate_sample_size(slope, intercept, distance):
+    predicted_size = 10**(slope * distance + intercept)
+    return predicted_size
