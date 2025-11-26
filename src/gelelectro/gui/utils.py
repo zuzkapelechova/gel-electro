0998e90ee8c9f6830d2ff9f99b1c55b53c9d8a87
Nikola Svobodnik <n_svobodnik@lad.ceitec.muni.cz>
2025-11-26 10:26:54 +0100

    img alignment

--- src/gelelectro/gui/utils.py
+++ src/gelelectro/gui/utils.py
@@ -241,3 +241,40 @@ def compute_sample_sizes(preprocessed_img, ladder2=False):
         results_text += "--------------\n"
 
     return grayscale_img, results_text
+
+def align_img(preprocessed_img):
+
+    grayscale_img = (preprocessed_img * 255).astype(np.uint8) if np.issubdtype(preprocessed_img.dtype, np.floating) else preprocessed_img.copy()
+    img_height, img_width = grayscale_img.shape
+
+    signals = get_signals_list(grayscale_img, img_width)
+
+    all_peak_centers = {}
+
+    for index, signal in enumerate(signals):
+        all_peak_centers[index] = get_peak_centers(signals, index, 5, 40)
+
+
+    max_peaks_in_line = max([len(all_peak_centers[i]) for i in range(len(all_peak_centers))])
+
+    all_lane_specifs, ladder_specifs = get_lane_and_ladder_specifs(width_threshold = 25, min_ladder_bands = 3, all_peak_centers = all_peak_centers)
+
+    # align the img based on the highest signal of each ladder
+    point1_x = ladder_specifs["center"]
+    point2_x = all_lane_specifs[max(all_lane_specifs.keys())]["center"]
+
+    point1_y = np.argmax(signals[int(point1_x)])
+    point2_y = np.argmax(signals[int(point2_x)])
+
+    delta_x = point2_x - point1_x
+    delta_y = point2_y - point1_y
+    angle = np.arctan(delta_y / delta_x) * 180/np.pi
+
+    mid_point = (int(point1_x + delta_x/2), int(point1_y + delta_y/2))
+
+
+    M = cv2.getRotationMatrix2D(center=mid_point, angle=angle, scale=1)
+    aligned =cv2.warpAffine(grayscale_img, M, dsize=(img_width, img_height))
+
+
+    return aligned
